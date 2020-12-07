f10fa9544c08b639745b99276b59181e62008817
Sobattan <ronan.petit@outlook.fr>
2020-12-07 21:18:38 +0100

    lolilol

---
+++ je-fais-des-tests/src/App.js
@@ -0,0 +1,16 @@
+import logo from './montre.svg';
+import logo2 from './aiguille.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo2} className="App-logo2" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
