6b121c80cc913e2605d621b3b72387f4bc246346
Sobattan <ronan.petit@outlook.fr>
2020-12-13 18:38:08 +0100

    On avance petit à petit

--- front_end/src/pages/Home.js
+++ front_end/src/pages/Home.js
@@ -1,5 +1,7 @@
 import { render } from '@testing-library/react';
 import React, {Component} from 'react';
+import '../styles/Home.css';
+import anime from 'animejs';
 
 class Home extends Component
 {
@@ -7,10 +9,48 @@ class Home extends Component
     {
         super(props);
     }
+    componentDidMount()
+    {
+        this.animation();
+    }
+    animation()
+    {
+        var element = document.getElementsByClassName("Nomdusite")[0];
+
+        element.innerHTML = element.textContent.replace(/\S/g,'<span class="letter">$&</span>');
+
+        anime.timeline({loop:true})
+        .add({
+        targets:'.Nomdusite .letter',
+        scale:[3,1],
+        opacity:[0,1],
+        translateZ:0,
+        duration:1000,
+        easing:"easeOutExpo",
+        delay:(elem, index) => index*70
+        })
+        .add({
+        targets:'.Nomdusite',
+        opacity:0,
+        duration:1000,
+        delay:1000,
+        easing:"easeOutExpo"
+        })
+    }
     render()
     {
         return(
-            <div>home</div>
+            <div className='Home'>
+                <div className='Bande'>
+                    <div className='Nomdusite'>
+                        La Société du jeu
+                    </div>
+                    <input className='Pseudo' type='text'/>
+                    <button className='Startbutton'>
+                        JOUER
+                    </button>
+                </div>
+            </div>
         )
     }
 }
